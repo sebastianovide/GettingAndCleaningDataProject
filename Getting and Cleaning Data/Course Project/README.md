5e21d381d9c601b7956816ce22aaca10329d81e7
Sebastian Ovide <Sebastian.Ovide@gmail.com>
2015-08-12 23:42:41 +0100

    question 1

---
+++ Getting and Cleaning Data/Course Project/README.md
@@ -0,0 +1,16 @@
+## Downloading the data
+```
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "Dataset.zip")
+unzip("Dataset.zip")
+```
+## Running the script
+It assumes that a ```combined``` folder doesn't exist. The script must run from the same folder from where the previous commands to download and unzip the data set were run.
+
+## 1
+I'm creating a new set of files in a new folder called ```combined```. As all the files have exaclty the same number of rows, we can safely append the files from the test folder into those of the data folder and save them into the new folder. 
+
+## 2
+## 3
+## 4
+## 5
+## 6
--- Getting and Cleaning Data/Course Project/run_analysis.R
+++ Getting and Cleaning Data/Course Project/run_analysis.R
@@ -1,2 +1,30 @@
-download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "Dataset.zip")
+# Download and unzip the data with:
+#download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "Dataset.zip")
+
+# just to make sure that we have the original data
+dataPath <- file.path("UCI HAR Dataset")
+unlink(dataPath, recursive = TRUE)
 unzip("Dataset.zip")
+
+
+### 1 ###
+
+combinedPath <- file.path(dataPath, "combined")
+testPath <- file.path(dataPath, "test")
+trainPath <- file.path(dataPath, "train")
+
+dir.create(combinedPath)
+dir.create(file.path(combinedPath, file.path("Inertial Signals")))
+
+# copy test files into combined folder
+fileListTest <- list.files(testPath, recursive = TRUE)
+filesFromTest <- file.path(testPath, fileListTest)
+filesToCombined <- file.path(combinedPath, gsub("(_test|_train)", "", fileListTest))
+file.copy(filesFromTest, filesToCombined)
+
+# append train files to the files in combined folder
+fileListTrain <- list.files(trainPath, recursive = TRUE)
+filesFromTrain <- file.path(trainPath, fileListTrain)
+file.append( filesToCombined, filesFromTrain )
+
+
