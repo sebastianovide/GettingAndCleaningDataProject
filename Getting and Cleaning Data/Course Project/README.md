17e8b9fbb02c3eba1069cd8df2d7342c7d122a77
Sebastian Ovide <Sebastian.Ovide@gmail.com>
2015-08-13 14:27:59 +0100

    2: Extracts only the measurements on the mean and standard deviation for each measurement

--- Getting and Cleaning Data/Course Project/README.md
+++ Getting and Cleaning Data/Course Project/README.md
@@ -4,12 +4,17 @@ download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUC
 unzip("Dataset.zip")
 ```
 ## Running the script
-It assumes that a ```combined``` folder doesn't exist. The script must run from the same folder from where the previous commands to download and unzip the data set were run.
+It assumes that a ```merged``` folder doesn't exist. The script must run from the same folder from where the previous commands to download and unzip the data set were run.
 
-## 1
-I'm creating a new set of files in a new folder called ```combined```. As all the files have exaclty the same number of rows, we can safely append the files from the test folder into those of the data folder and save them into the new folder. 
+## 1: Merges the training and the test sets to create one data set.
+I'm creating a new set of files in a new folder called ```merged```. As all the files have exaclty the same number of rows, we can safely append the files from the test folder into those of the data folder and save them into the new folder. 
 
-## 2
+## 2: Extracts only the measurements on the mean and standard deviation for each measurement 
+At this point I am combining ```subject.txt```, ```X.txt``` and ```y.txt``` into a single data table.
+The files in ```Inertial Signals``` are ignored as not requested for this excercise.  I
+From the file ```X.txt``` I'm extracting only those columns which label contains the workd "mean" or "std" case insensitive. I am probably adding extra columns, but as it is not clear what means and std are important, I prefer to have extra columns than too little.
+
+
 ## 3
 ## 4
 ## 5
--- Getting and Cleaning Data/Course Project/run_analysis.R
+++ Getting and Cleaning Data/Course Project/run_analysis.R
@@ -1,30 +1,64 @@
 # Download and unzip the data with:
 #download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "Dataset.zip")
 
-# just to make sure that we have the original data
+# just to make sure that we have the original data, remove the "UCI HAR Dataset" 
+# folder and unzip the "Dataset.zip" file. 
 dataPath <- file.path("UCI HAR Dataset")
 unlink(dataPath, recursive = TRUE)
 unzip("Dataset.zip")
 
-
 ### 1 ###
-
-combinedPath <- file.path(dataPath, "combined")
+# Merges the training and the test sets to create one data set.
+mergedPath <- file.path(dataPath, "merged")
 testPath <- file.path(dataPath, "test")
 trainPath <- file.path(dataPath, "train")
 
-dir.create(combinedPath)
-dir.create(file.path(combinedPath, file.path("Inertial Signals")))
+dir.create(mergedPath)
+dir.create(file.path(mergedPath, file.path("Inertial Signals")))
 
-# copy test files into combined folder
+# copy test files into merged folder
 fileListTest <- list.files(testPath, recursive = TRUE)
 filesFromTest <- file.path(testPath, fileListTest)
-filesToCombined <- file.path(combinedPath, gsub("(_test|_train)", "", fileListTest))
-file.copy(filesFromTest, filesToCombined)
+filesToMerged <- file.path(mergedPath, gsub("(_test|_train)", "", fileListTest))
+file.copy(filesFromTest, filesToMerged)
 
-# append train files to the files in combined folder
+# append train files to the files in merged folder
 fileListTrain <- list.files(trainPath, recursive = TRUE)
 filesFromTrain <- file.path(trainPath, fileListTrain)
-file.append( filesToCombined, filesFromTrain )
+file.append( filesToMerged, filesFromTrain )
 
 
+### 2 ###
+# Extracts only the measurements on the mean and standard deviation for each 
+# measurement. 
+
+# read original labels
+features <- read.table(file.path(dataPath, "features.txt"))
+names(features)[1] <- "ID"
+names(features)[2] <- "originalLabel" 
+
+# read original
+dt <- read.table(file.path(mergedPath, "X.txt"), col.names = features[,2])
+
+# remove columns which labels don't contain mean or std
+idxFeaturesWithMeanOrStd <- grep("mean|std", features[,2], ignore.case = TRUE)
+dt <- dt[,idxFeaturesWithMeanOrStd]
+
+# add y.txt file as y activity column
+activities<-read.table(file.path(mergedPath,"y.txt"))
+dt$activity <- activities
+
+# add subject.txt file as subjectNumber column
+subjectsId<-read.table(file.path(mergedPath,"subject.txt"))
+dt$subjectId <- subjectsId
+
+### CODE BOOK ###
+# TODO
+#descrip <- c("greendog", "bluecat")
+#explanation <- decrip
+#explanation <- gsub("green", "It is a green ", explanation)
+#explanation <- gsub("blue", "It is a blue ", explanation)
+#codebook <- paste("* ",descrip,"\n",explanation,"\n")
+#write.table(codebook, "codebook.md", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
+### write.table(dt[1:100,],file.path(mergedPath,"ciccio.txt"))
